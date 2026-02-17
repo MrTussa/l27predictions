bfa2ea7d8fa1af03f95db99a71fdaafc11fa592c
MrTussa <dd993736@gmail.com>
2026-02-17 05:46:40 +0500

    Removed: unused utilities and seo-plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,23 +1,3 @@
-import { seoPlugin } from '@payloadcms/plugin-seo'
-import { Plugin } from 'payload'
+import type { Plugin } from 'payload'
 
-import { getServerSideURL } from '@/utilities/getURL'
-
-const generateTitle = ({ doc }: { doc: { title?: string } }) => {
-  return doc?.title
-    ? `${doc.title} | L27 F1 Predictions`
-    : 'L27 - Formula 1 Predictions Championship'
-}
-
-const generateURL = ({ doc }: { doc: { slug?: string } }) => {
-  const url = getServerSideURL()
-
-  return doc?.slug ? `${url}/${doc.slug}` : url
-}
-
-export const plugins: Plugin[] = [
-  seoPlugin({
-    generateTitle,
-    generateURL,
-  }),
-]
+export const plugins: Plugin[] = []
